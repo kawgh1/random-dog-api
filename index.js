cb159a401c5429ae5db79ff850600b12e1060dc3
J J <krseven@gmail.com>
2022-03-01 17:22:53 -0600

    final

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// glocals
+
+let timer;
+let deleteFirstPhotoDelay;
+
+
 
 // fetch with Promises
 
@@ -8,7 +14,7 @@
 async function start() {
     const response = await fetch('https://dog.ceo/api/breeds/list/all');
     const data = await response.json();
-    // console.log(data)
+    // console.log(data.message)
     createBreedList(data.message)
 }
 
@@ -30,8 +36,39 @@ async function loadByBreed(breed) {
     if(breed != "Choose a dog breed"){
         const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
         const data = await response.json();
-        console.log(data.message)
+        // only take breeds with more than 5 images
+        console.log(data.message.length + " images of " + breed)
+        createSlideShow(data.message)
+        
     }
-    
+
+}
+
+function createSlideShow(images) {
+    let currentPosition = 0;
+    clearInterval(timer);
+    clearTimeout(deleteFirstPhotoDelay)
+    document.getElementById('slideshow').innerHTML = `
+        <div class="slide" style="background-image: url('${images[0]}')"></div>
+        <div class="slide" style="background-image: url('${images[1]}')"></div>
+    `
+
+    currentPosition += 2;
+    // timer
+    timer = setInterval(nextSlide, 3000);
+
+    function nextSlide() {
+        document.getElementById('slideshow').insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+        deleteFirstPhotoDelay = setTimeout(function(){
+            document.querySelector(".slide").remove()
+        }, 1000);
+
+        // increment currentPosition, but if it's the last image, reset back to 0 to start over
+        if(currentPosition + 1 >= images.length) {
+            currentPosition = 0;
+        } else {
+            currentPosition++;
+        }
+    }
 
 }
